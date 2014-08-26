ca1ceeaaffefd01178372483b878bc9cdbe5c19c
Amjad Saadeh <mail@amjadsaadeh.de>
2014-08-26 12:18:29 +0200

    begin test for grid fitting and remove some useless ellipses
    Former-commit-id: 1647c20cf6502b42c6b3c12daa93fde141f969e1

--- test/GridFitterTest.cpp
+++ test/GridFitterTest.cpp
@@ -7,9 +7,12 @@
 
 #include "GridFitterTest.h"
 
+using namespace boost::filesystem;
+using namespace boost::archive;
+using namespace cv;
+
 GridFitterTest::GridFitterTest() {
 	// TODO Auto-generated constructor stub
-
 }
 
 GridFitterTest::~GridFitterTest() {
@@ -18,13 +21,38 @@ GridFitterTest::~GridFitterTest() {
 
 // TODO ich brauch noch nutzbare Gridpositionen and Winkel  als vergleich!!! Und ich sollte auch die Ellipsen filtern!!!
 void GridFitterTest::SetUp() {
-	// TODO Load serialized ellipses and Grid Positions and Angles
+
 }
 
 void GridFitterTest::TearDown() {
 	// TODO Free serialized ellipses
 }
 
-TEST(GridFitterTest, TestGridPoisitions) {
+TEST(GridFitterTest, TestGridPositions) {
 	// TODO Testrahmen: 90% der Grids sollten schon nahe der Position sein, die erwartet wird (abweichung von einigen Pixeln und 0.5 bis Grad im Winkel könnte man ja druchgehen lassen)
+		path full_path("./test/data/grid_fitter_ellipses/");
+
+		directory_iterator end_iter;
+		for (directory_iterator dir_itr(full_path); dir_itr != end_iter; ++dir_itr) {
+			try {
+				if (is_regular_file(dir_itr->status())) {
+					ifstream file(dir_itr->path().string().c_str());
+					text_iarchive ia(file);
+					vector<Ellipse> ellipses;
+					ia >> ellipses;
+					file.close();
+					for (unsigned int i = 0; i < ellipses.size(); i++) {
+						stringstream ss;
+						ss << dir_itr->path().filename() << " " << i;
+						namedWindow(ss.str());
+						imshow(ss.str(), ellipses[i].transformedImage);
+					}
+					while (waitKey() != 10);
+					destroyAllWindows();
+		        }
+			}
+			catch (const std::exception & ex) {
+				std::cout << dir_itr->path().filename() << " " << ex.what() << std::endl;
+			}
+		}
 }
--- test/GridFitterTest.h
+++ test/GridFitterTest.h
@@ -9,6 +9,14 @@
 #define GRIDFITTERTEST_H_
 
 #include "gtest/gtest.h"
+#include <boost/filesystem.hpp>
+#include "../src/decoder/GridFitter.h"
+#include "../src/decoder/datastructure/Grid.h"
+#include "../src/decoder/datastructure/Ellipse.h"
+#include "../src/serialization.h"
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
 
 class GridFitterTest: public testing::Test {
 public:
