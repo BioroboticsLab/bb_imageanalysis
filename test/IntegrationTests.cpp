2d4f479e9c288ae0e6d98b9351407f14c1e0e373
Benjamin Wild <b.w@fu-berlin.de>
2015-09-14 14:56:52 +0200

    reenable tests

--- test/IntegrationTests.cpp
+++ test/IntegrationTests.cpp
@@ -1,14 +1,17 @@
+#include <array>
 #include <vector>
 
 #include <boost/filesystem.hpp>
 
 #include "catch.hpp"
 
-#include "src/pipeline/Preprocessor.h"
-#include "src/pipeline/Localizer.h"
-#include "src/pipeline/EllipseFitter.h"
-#include "src/pipeline/GridFitter.h"
-#include "src/pipeline/Decoder.h"
+#include <pipeline/Preprocessor.h>
+#include <pipeline/Localizer.h>
+#include <pipeline/EllipseFitter.h>
+#include <pipeline/GridFitter.h>
+#include <pipeline/Decoder.h>
+
+#include <pipeline/settings/Settings.h>
 
 TEST_CASE("Pipeline finishes with test data and returns results", "[integration]") {
 	pipeline::Preprocessor preprocessor;
@@ -17,12 +20,41 @@ TEST_CASE("Pipeline finishes with test data and returns results", "[integration]
 	pipeline::GridFitter gridFitter;
 	pipeline::Decoder decoder;
 
-	for (boost::filesystem::recursive_directory_iterator end, dir(TEST_DATA_PATH); dir != end; ++dir ) {
+    boost::filesystem::path basePath(TEST_DATA_PATH);
+    boost::filesystem::path configFile = basePath / "settings.json";
+
+    if (boost::filesystem::is_regular_file(configFile)) {
+        pipeline::settings::preprocessor_settings_t preprocessor_settings;
+        pipeline::settings::localizer_settings_t localizer_settings;
+        pipeline::settings::ellipsefitter_settings_t ellipsefitter_settings;
+        pipeline::settings::gridfitter_settings_t gridfitter_settings;
+
+        for (pipeline::settings::settings_abs* settings :
+             std::array<pipeline::settings::settings_abs*, 4>({&preprocessor_settings,
+                                                               &localizer_settings,
+                                                               &ellipsefitter_settings,
+                                                               &gridfitter_settings}))
+        {
+            settings->loadFromJson(configFile.string());
+        }
+
+        // TODO: don't test deeplocalizer for now
+        localizer_settings.setValue(pipeline::settings::Localizer::Params::DEEPLOCALIZER_FILTER, false);
+
+        preprocessor.loadSettings(preprocessor_settings);
+        localizer.loadSettings(localizer_settings);
+        ellipseFitter.loadSettings(ellipsefitter_settings);
+        gridFitter.loadSettings(gridfitter_settings);
+    } else {
+        REQUIRE(false);
+    }
+
+    for (boost::filesystem::recursive_directory_iterator end, dir(basePath); dir != end; ++dir ) {
 		if (boost::filesystem::is_regular_file(*dir)) {
 			if (boost::filesystem::extension(*dir) == ".jpeg") {
 				INFO(*dir);
 
-				cv::Mat img = cv::imread((*dir).path().string());
+                cv::Mat img = cv::imread((*dir).path().string(), CV_LOAD_IMAGE_GRAYSCALE);
 				cv::Mat preprocessedImg;
 				std::vector<pipeline::Tag> taglist;
 				preprocessedImg = preprocessor.process(img);
